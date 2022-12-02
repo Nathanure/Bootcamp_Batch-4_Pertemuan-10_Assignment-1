46b3175577087156d517dae1ca61a44995626378
Nathanure <nathancadankelas6a@gmail.com>
2022-12-02 11:31:10 +0700

    Forgot to add comment

--- app.js
+++ app.js
@@ -4,25 +4,33 @@ const fs = require('fs');
 
 // Function for http response to path directory
 function page(dirPath, res) {
+    // read the file as what the directory path wants
     fs.readFile(dirPath, (err, data) => {
         if (err) {
+            // writehead is to determine the state of website
             res.writeHead(404);
+            // write is to tell the content of website
             res.write('Error : Page Not Found');
         } else {
             res.write(data);
         }
+        // end is to close and stop the buffering
         res.end();
     })
 }
 
-// Creating a server
+// Calling http module
 http
+    // Creating a server
     .createServer((req, res) => {
+        // Variable for request in the url
         const url = req.url;
+        // result for the statement of web, and its type of content
         res.writeHead(200, {
             'Content-type': 'text/html'
         })
         console.log(url);
+        // the result and branch of inputted url
         if (url === '/about') {
             page('./page/about.html', res)
         } else if (url === '/contact') {
@@ -31,6 +39,7 @@ http
             page('./page/index.html', res)
         }
     })
+    // Output of inserted request happening on the web 
     .listen(3000, () => {
         console.log('Server is listening on port 3000');
     })
